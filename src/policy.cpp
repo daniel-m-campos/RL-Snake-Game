57a85cece03f342ec174e8b68ca622aa8cbaa7ab
Daniel M Campos <daniel.m.campos@icloud.com>
2021-02-13 21:36:56 -0600

    Redesign Policy to have an ActionValuer
    - Policy's is now only responsibly for generating and action from a
    state and reporting the Probability
    - The iterator responsibility was moved to ActionValuer for better
    cohesion

--- src/policy.cpp
+++ src/policy.cpp
@@ -1,116 +1,41 @@
 #include "policy.h"
 
 #include <algorithm>
-#include <list>
-#include <numeric>
 
 template <typename S, typename A>
-EpsilonGreedy<S, A>::EpsilonGreedy(size_t num_states, size_t num_actions,
-                                   double epsilon)
-    : _epsilon(epsilon),
-      _num_states(num_states),
-      _num_actions(num_actions),
-      _engine(std::random_device()()),
-      _uniform_dist(std::uniform_real_distribution<>(0, 1)) {
-  CacheActions();
-  std::vector<A> actions(num_actions);
-  std::iota(actions.begin(), actions.end(), 0);
-  for (S state = 0; state < num_states; ++state) {
-    _state_actions[state] = actions;
-  }
-}
+EpsilonGreedy<S, A>::EpsilonGreedy(
+    std::unique_ptr<ActionValuer<S, A>> action_valuer, double epsilon)
+    : _epsilon(epsilon), _action_valuer(std::move(action_valuer)) {}
 
 template <typename S, typename A>
 EpsilonGreedy<S, A>::EpsilonGreedy(
     EpsilonGreedy::state_action_map state_actions, double epsilon)
     : _epsilon(epsilon),
-      _num_states(state_actions.size()),
-      _state_actions(state_actions),
       _uniform_dist(std::uniform_real_distribution<>(0, 1)),
       _engine(std::random_device()()) {
-  CacheActions();
-  _num_actions = _actions.size();
-}
-
-template <typename S, typename A>
-void EpsilonGreedy<S, A>::CacheActions() {
-  for (const auto& [state, actions] : _state_actions) {
-    for (const auto& action : actions) {
-      _actions[action] = true;
-    }
-  }
+  _action_valuer = std::make_unique<SimpleActionValuer<S, A>>(state_actions);
 }
 
 template <typename S, typename A>
 double EpsilonGreedy<S, A>::Probability(A action, S state) {
-  auto actions = GreedyAction(state);
-  double num_actions = _state_actions[state].size();
+  auto actions = _action_valuer->ArgMax(state);
+  double num_actions = _action_valuer->GetActions(state).size();
   if (std::find(actions.begin(), actions.end(), action) != actions.end()) {
     return _epsilon / num_actions + (1 - _epsilon) / actions.size();
   } else {
     return _epsilon / num_actions;
   }
 }
-
-template <typename S, typename A>
-double EpsilonGreedy<S, A>::GetValue(S state, A action) {
-  return _action_values[{state, action}];
-}
-
 template <typename S, typename A>
 A EpsilonGreedy<S, A>::operator()(S state) {
   std::vector<A> actions;
   if (_uniform_dist(_engine) < _epsilon) {
-    actions = _state_actions[state];
+    actions = _action_valuer->GetStates();
   } else {
-    actions = GreedyAction(state);
+    actions = _action_valuer->ArgMax(state);
   }
   std::shuffle(actions.begin(), actions.end(), _engine);
   return actions[0];
-}
-
-template <typename S, typename A>
-size_t EpsilonGreedy<S, A>::StateSize() const {
-  return _num_states;
-}
-
-template <typename S, typename A>
-size_t EpsilonGreedy<S, A>::ActionSize() const {
-  return _num_actions;
-}
-
-template <typename S, typename A>
-std::vector<A> EpsilonGreedy<S, A>::GreedyAction(S state) {
-  auto actions = GetActions(state);
-  std::sort(
-      actions.begin(), actions.end(), [&](const auto& left, const auto& right) {
-        return _action_values[{state, left}] > _action_values[{state, right}];
-      });
-  const auto max_value = _action_values[{state, actions[0]}];
-  auto iter = actions.begin();
-  while (iter != actions.end()) {
-    if (_action_values[{state, *iter}] != max_value) {
-      iter = actions.erase(iter);
-    } else {
-      ++iter;
-    }
-  }
-  return actions;
-}
-
-template <typename S, typename A>
-std::vector<A> EpsilonGreedy<S, A>::GetActions(const S& state) {
-  std::vector<A> greedy_actions;
-  greedy_actions.reserve(_state_actions[state].size());
-  for (const auto& action : _state_actions[state]) {
-    greedy_actions.push_back(action);
-  }
-  return greedy_actions;
-}
-
-template <typename S, typename A>
-void EpsilonGreedy<S, A>::SetValue(S state, A action, double value) {
-  _action_values[{state, action}] = value;
 }
 
 template class EpsilonGreedy<int, int>;
--- tests/policy_tests.cpp
+++ tests/policy_tests.cpp
@@ -7,10 +7,6 @@ class MockPolicy : public Policy<int, int> {
   double Probability(int action, int state) override {
     return action == -state;
   }
-  double GetValue(int state, int action) override { return 0; }
-  void SetValue(int state, int action, double value) override {}
-  size_t StateSize() const override { return std::numeric_limits<int>::max(); }
-  size_t ActionSize() const override { return std::numeric_limits<int>::max(); }
 };
 
 class PolicyFixture : public ::testing::Test {
@@ -25,34 +21,6 @@ TEST_F(PolicyFixture, TestMockPolicyProbability) {
   EXPECT_DOUBLE_EQ(policy.Probability(2, -2), 1.0);
 }
 
-class EpsilonGreedyZeroValueFunctionFixture : public ::testing::Test {
- public:
-  double epsilon = 0.1;
-  EpsilonGreedy<int, int> policy{3, 4, epsilon};
-};
-
-TEST_F(EpsilonGreedyZeroValueFunctionFixture, TestInitializedValue) {
-  for (int action = 0; action < policy.ActionSize(); ++action) {
-    for (int state = 0; state < policy.StateSize(); ++state) {
-      EXPECT_DOUBLE_EQ(policy.GetValue(action, state), 0.0);
-    }
-  }
-}
-
-TEST_F(EpsilonGreedyZeroValueFunctionFixture, TestInitializedProbability) {
-  for (int action = 0; action < policy.ActionSize(); ++action) {
-    for (int state = 0; state < policy.StateSize(); ++state) {
-      EXPECT_NEAR(policy.Probability(action, state), 1.0 / policy.ActionSize(),
-                  1e-6);
-    }
-  }
-}
-
-TEST_F(EpsilonGreedyZeroValueFunctionFixture,
-       TestOperatorCallLessThanActionSize) {
-  EXPECT_LT(policy(2), policy.ActionSize());
-}
-
 class EpsilonGreedyUniqueGreedyActionPerStateFixture : public ::testing::Test {
  public:
   std::unordered_map<int, std::vector<int>> state_actions{
@@ -60,18 +28,16 @@ class EpsilonGreedyUniqueGreedyActionPerStateFixture : public ::testing::Test {
       {1, {1, 3}},
       {2, {2, 4}},
   };
-  std::unordered_map<std::pair<int, int>, double> action_values{
+  std::unordered_map<std::pair<int, int>, double> action_value_map{
       {{0, 0}, 0}, {{0, 1}, 1}, {{1, 1}, 0},
       {{1, 3}, 1}, {{2, 2}, 0}, {{2, 4}, 1},
   };
+
   EpsilonGreedy<int, int> create_policy(double epsilon) {
-    EpsilonGreedy<int, int> policy{state_actions, epsilon};
-    for (const auto& [state, actions] : state_actions) {
-      for (const auto& action : actions) {
-        policy.SetValue(state, action, action_values[{state, action}]);
-      }
-    }
-    return policy;
+    return EpsilonGreedy<int, int>{
+        std::make_unique<SimpleActionValuer<int, int>>(action_value_map),
+        epsilon};
+    ;
   }
 };
 
@@ -91,24 +57,4 @@ TEST_F(EpsilonGreedyUniqueGreedyActionPerStateFixture,
   auto policy = create_policy(epsilon);
   EXPECT_EQ(policy.Probability(0, state), epsilon * 1 / N);
   EXPECT_EQ(policy.Probability(1, state), epsilon * 1 / N + (1 - epsilon) * 1);
-}
-
-TEST_F(EpsilonGreedyUniqueGreedyActionPerStateFixture,
-       TestValueWithSingleGreedyAction) {
-  auto policy = create_policy(0.1);
-  for (const auto& [key, value] : action_values) {
-    auto [state, action] = key;
-    EXPECT_EQ(policy.GetValue(state, action), value);
-  }
-}
-
-TEST_F(EpsilonGreedyUniqueGreedyActionPerStateFixture, TestSetValues) {
-  auto policy = create_policy(0.1);
-  double new_value = 42.0;
-  for (const auto& [key, value] : action_values) {
-    auto [state, action] = key;
-    policy.SetValue(state, action, new_value);
-    EXPECT_EQ(policy.GetValue(state, action), new_value);
-    ++new_value;
-  }
 }
