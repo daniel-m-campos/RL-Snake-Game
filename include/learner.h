3823dce1b2533ee42ffc7423d324b5abe7c5f9b5
Daniel M Campos <daniel.m.campos@icloud.com>
2021-02-14 10:57:40 -0600

    Add QLearner

--- include/learner.h
+++ include/learner.h
@@ -7,17 +7,22 @@ template <typename S, typename A, typename R>
 class Learner {
  public:
   virtual ~Learner() = default;
-  virtual void Reinforce(Policy<S, A>&, S, A, R, S, A) = 0;
+  virtual void Reinforce(const Policy<S, A>&, ActionValuer<S, A>&, S, A, R,
+                         S) = 0;
 };
 
 template <typename S, typename A, typename R>
 class QLearner : public Learner<S, A, R> {
  public:
   QLearner(double discount_factor, double step_size);
+  void Reinforce(const Policy<S, A>& policy, ActionValuer<S, A>& valuer,
+                 S state, A action, R reward, S new_state) override;
 
  private:
   double _discount_factor;
   double _step_size;
+
+  double NewValue(double old_value, R reward, double greedy_value) const;
 };
 
 #endif  // RLSNAKEGAME_LEARNER_H
--- tests/learner_tests.cpp
+++ tests/learner_tests.cpp
@@ -9,27 +9,46 @@ class MockPolicy : public Policy<S, A> {
  public:
   MOCK_METHOD(A, ParentheisesOp, (S state));
   A operator()(S state) override { return ParentheisesOp(state); }
+  MOCK_METHOD(double, Probability, (A action, S given_state), (override));
+};
+
+template <typename S, typename A>
+class MockActionValuer : public ActionValuer<S, A> {
+ public:
   MOCK_METHOD(double, GetValue, (S state, A action), (override));
   MOCK_METHOD(void, SetValue, (S state, A action, double value), (override));
-  MOCK_METHOD(double, Probability, (A action, S given_state), (override));
-  MOCK_METHOD(size_t, StateSize, (), (const, override));
-  MOCK_METHOD(size_t, ActionSize, (), (const, override));
+  MOCK_METHOD(std::vector<A>, ArgMax, (S state), (override));
+  MOCK_METHOD(std::vector<S>, GetStates, (), (override));
+  MOCK_METHOD(std::vector<A>, GetActions, (S state), (override));
 };
 
 TEST(LearnerTest, TestInstanitateMockPolicy) { MockPolicy<int, int> policy; }
 
-//TEST(QLearnerTest, TestPolicyUpdatedWithUniqueGreedyAction) {
-//  auto state = 0;
-//  auto action = 0;
-//  auto reward = -1.0;
-//  auto new_state = 1;
-//  auto new_action = 1;
-//  auto value = 42.0;
-//  auto discount_factor = 0.95;
-//  auto step_size = 0.25;
-//
-//  MockPolicy<int, int> policy;
-//  EXPECT_CALL(policy, GetValue(0, 0)).WillOnce(Return(value));
-//  EXPECT_CALL(policy, GetValue(0, 0)).WillOnce(Return(value));
-////  EXPECT_CALL(policy, SetValue(0, 0, )).WillOnce();
-//}
+TEST(QLearnerTest, TestPolicyUpdatedWithUniqueGreedyAction) {
+  auto state = 0;
+  auto action = 0;
+  auto reward = -1.0;
+  auto new_state = 1;
+  auto greedy_action = 2;
+  auto greedy_value = 7;
+  auto value = 42.0;
+  auto discount_factor = 0.95;
+  auto step_size = 0.25;
+
+  MockPolicy<int, int> policy;
+  MockActionValuer<int, int> valuer;
+  EXPECT_CALL(valuer, GetValue(state, action)).WillOnce(Return(value));
+  EXPECT_CALL(valuer, ArgMax(new_state))
+      .WillOnce(Return(std::vector{greedy_action}));
+  EXPECT_CALL(valuer, GetValue(new_state, greedy_action))
+      .WillOnce(Return(greedy_value));
+  EXPECT_CALL(
+      valuer,
+      SetValue(state, action,
+               value + step_size *
+                           (reward + discount_factor * greedy_value - value)))
+      .WillOnce(Return());
+
+  QLearner<int, int, double> learner{discount_factor, step_size};
+  learner.Reinforce(policy, valuer, state, action, reward, new_state);
+}
