2630df40517e73bdeb0213b8adef9914ed397ad3
Daniel M Campos <daniel.m.campos@icloud.com>
2021-02-15 15:16:05 -0600

    Fix signatures

--- include/environment.h
+++ include/environment.h
@@ -1,13 +1,13 @@
 #ifndef RLSNAKEGAME_ENVIRONMENT_H
 #define RLSNAKEGAME_ENVIRONMENT_H
 
-template <typename S, typename A, typename R>
+template <typename S, typename A>
 class Environment {
  public:
   virtual ~Environment() = default;
   virtual void Update(A) = 0;
-  virtual S GetState() const = 0;
-  virtual R GetReward() const = 0;
+  virtual S GetState() = 0;
+  virtual double GetReward() = 0;
 };
 
 #endif  // RLSNAKEGAME_ENVIRONMENT_H
