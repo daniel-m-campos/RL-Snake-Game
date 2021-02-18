555f725a9b71b4c0e4579b839f8a77b0d3b5c77b
Daniel M Campos <daniel.m.campos@icloud.com>
2021-02-17 21:45:55 -0600

    Add value function test data

--- tests/integration/test.cpp
+++ tests/integration/test.cpp
@@ -73,8 +73,21 @@ TEST(TestGridWorldSimulation, TestSimulationLoop) {
       {{4, 4}, Move::kWest},
   }};
 
+  // Data from Figure 3.5 page 65 of Sutton and Bart0 2018
+  std::unordered_map<Position, double> value_function{{
+      {{0, 0}, 14.4}, {{0, 1}, 16.0}, {{0, 2}, 17.8}, {{0, 3}, 19.8},
+      {{0, 4}, 22.0}, {{1, 0}, 16.0}, {{1, 1}, 17.8}, {{1, 2}, 19.8},
+      {{1, 3}, 22.0}, {{1, 4}, 24.4}, {{2, 0}, 14.4}, {{2, 1}, 16.0},
+      {{2, 2}, 17.8}, {{2, 3}, 19.8}, {{2, 4}, 22.0}, {{3, 0}, 13.0},
+      {{3, 1}, 14.4}, {{3, 2}, 16.0}, {{3, 3}, 17.8}, {{3, 4}, 19.4},
+      {{4, 0}, 11.7}, {{4, 1}, 13.0}, {{4, 2}, 14.4}, {{4, 3}, 16.0},
+      {{4, 4}, 17.5},
+  }};
+
   for (const auto& [state, expected_action] : optimal_policy) {
     auto actual_action = action_valuer->ArgMax(state)[0];
     EXPECT_EQ(expected_action, actual_action);
+    auto actual_value = action_valuer->GetValue(state, actual_action);
+    EXPECT_NEAR(value_function[state], actual_value, 0.1);
   }
 }
