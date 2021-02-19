9f9b941a9fedc4b261314f39b4b940f6636d3586
Daniel M Campos <daniel.m.campos@icloud.com>
2021-02-18 20:48:59 -0600

    Add SimpleStateActionMap

--- include/state_action_map.h
+++ include/state_action_map.h
@@ -1,6 +1,7 @@
 #ifndef RLSNAKEGAME_STATEACTIONMAP_H
 #define RLSNAKEGAME_STATEACTIONMAP_H
 
+#include <set>
 #include <unordered_map>
 #include <vector>
 
@@ -24,6 +25,21 @@ class StateActionHashMap : public StateActionMap<S, A> {
  private:
   state_action_map _state_actions;
   std::vector<S> _states;
+};
+
+template <typename S, typename A>
+class SimpleStateActionMap : public StateActionMap<S, A> {
+ public:
+  SimpleStateActionMap() = delete;
+  explicit SimpleStateActionMap(std::set<S> states, std::set<A> actions);
+  const std::vector<S>& GetStates() override;
+  const std::vector<A>& GetActions(S state) override;
+
+ private:
+  std::set<S> _states;
+  std::vector<S> _unique_states;
+  std::set<A> _actions;
+  std::vector<A> _unique_actions;
 };
 
 #endif  // RLSNAKEGAME_STATEACTIONMAP_H
--- src/state_action_map.cpp
+++ src/state_action_map.cpp
@@ -23,4 +23,26 @@ const std::vector<A>& StateActionHashMap<S, A>::GetActions(S state) {
   return _state_actions.at(state);
 }
 
+template <typename S, typename A>
+SimpleStateActionMap<S, A>::SimpleStateActionMap(std::set<S> states,
+                                                 std::set<A> actions)
+    : _states{states}, _actions{actions} {
+  std::copy(_actions.begin(), _actions.end(),
+            std::back_inserter(_unique_actions));
+}
+
+template <typename S, typename A>
+const std::vector<A>& SimpleStateActionMap<S, A>::GetActions(S state) {
+  _states.insert(state);
+  return _unique_actions;
+}
+
+template <typename S, typename A>
+const std::vector<S>& SimpleStateActionMap<S, A>::GetStates() {
+  _unique_states.clear();
+  std::copy(_states.begin(), _states.end(), std::back_inserter(_unique_states));
+  return _unique_states;
+}
+
 template class StateActionHashMap<int, int>;
+template class SimpleStateActionMap<int, int>;
