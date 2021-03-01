db3e198be19110aa8431acc0bbbcf251c4ea00ad
Daniel M Campos <daniel.m.campos@icloud.com>
2021-02-28 19:08:03 -0600

    Remove unused method

--- include/action_valuer_factory.h
+++ include/action_valuer_factory.h
@@ -8,9 +8,6 @@ class ActionValuerFactory {
  public:
   static std::unique_ptr<ActionValuer<S, A>> CreateSimpleActionValuer(
       const std::set<S>& states, const std::set<A>& actions);
-
-  static std::unique_ptr<ActionValuer<S, A>> CreateSimpleActionValuer(
-      std::unordered_map<S, std::vector<A>> state_action_map);
 };
 
 template <typename S, typename A>
@@ -19,14 +16,6 @@ ActionValuerFactory<S, A>::CreateSimpleActionValuer(
     const std::set<S>& states, const std::set<A>& actions) {
   return std::make_unique<SimpleActionValuer<S, A>>(
       std::make_unique<SimpleStateActionMap<S, A>>(states, actions));
-}
-
-template <typename S, typename A>
-std::unique_ptr<ActionValuer<S, A>>
-ActionValuerFactory<S, A>::CreateSimpleActionValuer(
-    std::unordered_map<S, std::vector<A>> state_action_map) {
-  return std::make_unique<SimpleActionValuer<S, A>>(
-      std::make_unique<StateActionHashMap<S, A>>(state_action_map));
 }
 
 #endif  // RLSNAKEGAME_ACTION_VALUER_FACTORY_H
